51f1abe912b585903aab0473c7ae9557a5ed91d7
Jade Manrique <jade.a.manrique@gmail.com>
2018-03-13 12:59:14 -0700

    Added more files

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,41 @@ $(document).ready( function () {
 
 
 
-  $("#article div").click( function (e) {
+  $("#article div img").click( function () {
+
+    $("#lightbox").show();
+
+    var imageLink = $(this).attr("src");
+
+    var imageText = "<img src='" + imageLink + "' />";
+
+    $("#lightbox").html(imageText);
+
+    $("#lightbox").click( function (event) {
+
+      $("#lightbox").hide();
+
+      });
+
+    });
+
+    $("#article div").hover( function(e) {
+
+          $(this).animate({opacity: 0.5}, 150);
+
+      }, function() {
+
+          $(this).animate({opacity: 1.0}, 150);
+
+      });
+
+
+  });
+
+
+/*
+
+$("#article div").click( function (e) {
 
     if ( $(this).find("img").css("width") == "100px" ) {
 
@@ -16,15 +50,5 @@ $(document).ready( function () {
 
   });
 
-  $("#article div").hover( function(e) {
 
-        $(this).animate({opacity: 0.5}, 200);
-
-    }, function() {
-
-        $(this).animate({opacity: 1.0}, 200);
-
-    });
-
-
-});
+*/
