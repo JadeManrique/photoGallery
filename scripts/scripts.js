066ff2c43d20867a9c0c01e24bb034495cfd61ed
Jade Manrique <jade.a.manrique@gmail.com>
2018-03-14 12:30:28 -0700

    Polishing up design
    git status

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,29 +4,22 @@ $(document).ready( function () {
 
   $("#article div img").click( function () {
 
-  //  $("#lightbox").show();
-
     var imageLink = $(this).attr("src");
 
     var imageText = "<img src=\'" + imageLink + "\' />";
 
     $("#lightbox").html(imageText);
 
-//    $("#lightbox").click( function (event) {
-
-//      $("#lightbox").hide();
-
-//      });
-
     });
+
 
     $("#article div img").hover( function(e) {
 
-          $(this).animate({opacity: 0.5, height: "auto", width: "120px"}, 150);
+          $(this).animate({opacity: 1.0, height: "auto", width: "120px"}, 150);
 
       }, function() {
 
-          $(this).animate({opacity: 1.0, height: "auto", width: "100px"}, 150);
+          $(this).animate({opacity: 0.5, height: "auto", width: "100px"}, 150);
 
       });
 
