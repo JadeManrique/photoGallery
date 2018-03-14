ebac4da8125025f6c534af984b500891b77df2f0
Jade Manrique <jade.a.manrique@gmail.com>
2018-03-13 17:29:47 -0700

    Fixed photo Gallery

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,32 +1,46 @@
 $(document).ready( function () {
 
+  $("#article div").click( function (e) {
 
+      if ( $(this).find("img").css("width") == "120px" ) {
 
-  $("#article div img").click( function () {
+       $(this).find("img").css("width", "200px");
 
-    $("#lightbox").show();
+      } else {
 
-    var imageLink = $(this).attr("src");
+        $(this).find("img").css("width", "100px");
 
-    var imageText = "<img src='" + imageLink + "' />";
-
-    $("#lightbox").html(imageText);
-
-    $("#lightbox").click( function (event) {
-
-      $("#lightbox").hide();
-
-      });
+      }
 
     });
 
-    $("#article div").hover( function(e) {
 
-          $(this).animate({opacity: 0.5}, 150);
+
+  // $("#article div img").click( function () {
+  //
+  //   $("#lightbox").show();
+  //
+  //   var imageLink = $(this).attr("src");
+  //
+  //   var imageText = "<img src='" + imageLink + "' />";
+  //
+  //   $("#lightbox").html(imageText);
+  //
+  //   $("#lightbox").click( function (event) {
+  //
+  //     $("#lightbox").hide();
+  //
+  //     });
+  //
+  //   });
+
+    $("#article div img").hover( function(e) {
+
+          $(this).animate({opacity: 0.5, height: "auto", width: "120px"}, 150);
 
       }, function() {
 
-          $(this).animate({opacity: 1.0}, 150);
+          $(this).animate({opacity: 1.0, height: "auto", width: "100px"}, 150);
 
       });
 
